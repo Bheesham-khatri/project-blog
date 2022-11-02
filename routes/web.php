94d3f36e3344dba43ae349a78eefc598ccc49829
Bheesham Khatri <bheesham.khatri@nextgeni.com>
2022-11-02 18:29:56 +0500

    edit route middleware

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,13 @@ Route::group(['middleware'=>['auth']], function(){
     Route::get('/dashboard',[PostController::class,'dashboard'
     ])->name('dashboard');
 
-Route::resource('categories',CategoryController::class,);
+Route::resource('categories',CategoryController::class,)->middleware('is_admin');
 Route::resource('posts',PostController::class);
 
 
 });
+
+
 
 
 require __DIR__.'/auth.php';
